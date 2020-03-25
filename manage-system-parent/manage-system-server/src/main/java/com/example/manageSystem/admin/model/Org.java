c6c17fe5669c020331c998a816328fa84957ec34
suahe <1054599027@qq.com>
2020-03-25 20:55:46 +0800

    组织实体

---
+++ manage-system-parent/manage-system-server/src/main/java/com/example/manageSystem/admin/model/Org.java
@@ -0,0 +1,126 @@
+package com.example.manageSystem.admin.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Table(name="sys_org")
+public class Org implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private  Integer orgId ;//
+    private  Integer parentId ;//上级ID
+    @Column(name = "org_code")
+    private  String orgCode ;//部门编码
+    private  String industryCategory ;//行业类型
+    private  String orgName ;//部门名称
+    private  String creator ;//L创建人
+    private  Date createTime ;//创建时间
+    private  String county ;//所属区
+    private  String street;//所属街道/镇
+    private  String neighborhood;//所属居委会/村
+    private  Integer itemId ;//项目id
+
+    private List<Org> children = new ArrayList<Org>();
+
+    public Integer getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(Integer orgId) {
+        this.orgId = orgId;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getOrgCode() {
+        return orgCode;
+    }
+
+    public void setOrgCode(String orgCode) {
+        this.orgCode = orgCode;
+    }
+
+    public String getIndustryCategory() {
+        return industryCategory;
+    }
+
+    public void setIndustryCategory(String industryCategory) {
+        this.industryCategory = industryCategory;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public List<Org> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Org> children) {
+        this.children = children;
+    }
+}
