c6c44100ee46cc9c65814187d956ccd5d73f42ad
suahe <1054599027@qq.com>
2020-03-30 23:20:59 +0800

    提交前端配置

--- vue-manage-system-master/config/index.js
+++ vue-manage-system-master/config/index.js
@@ -1,12 +1,24 @@
 var path = require('path')
 
 module.exports = {
-  devServer: {
-    proxy: {
-      '/': {
-        target: 'http://127.0.0.1:9011',
-        changeOrigin: true,
-      }
-    }
+  dev: {
+    env: require('./dev.env'),
+    port: 8088,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {
+      '/api':{
+        target:'http://127.0.0.1:8090',//本地
+        changeOrigin:true,
+      },
+      /*'/sockjs':{
+        // target:'http://192.168.102.164:8090/',
+        target:'http://127.0.0.1:8090',
+        changeOrigin:true,
+        pathRewrite: {'^/sockjs' : ''}
+      },*/
+    },
+    cssSourceMap: false
   }
 }
